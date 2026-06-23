7377b682a762d91955ce9c19ce83f66b69cd407c
pgougne <paris.gougne.05@gmail.com>
2026-06-23 13:51:01 +0200

    wall visualisation & ground

--- src/GameState.py
+++ src/GameState.py
@@ -3,6 +3,9 @@ from src.utils_io import load_json_file
 from src.core.Level import Level
 from src.core.LevelGenerator import LevelGenerator
 from typing import List
+from ursina import Ursina, Entity, Vec3, color, camera
+from src.core.Node import Node
+
 
 class GameState():
     def __init__(self,
@@ -13,7 +16,7 @@ class GameState():
                  points_per_ghost,
                  seed,
                  level_max_time
-                 ):
+                 ) -> None:
         self.highscore_filename_config = highscore_filename
         self.levels_config = levels
         self.lives_config = lives
@@ -28,9 +31,10 @@ class GameState():
             print(e)
         print(self.levels[0].level_map)
         print("\n\n", self.scores)
+        self.game()
 
     @staticmethod
-    def _getScores(filename: str):
+    def _getScores(filename: str) -> ScoresList:
         return ScoresList(**load_json_file(filename))
 
     @staticmethod
@@ -39,3 +43,47 @@ class GameState():
         for level in levels_config:
             levels.append(LevelGenerator().generate_level(level))
         return levels
+
+    @staticmethod
+    def createMap(mapData: Level):
+        ground = Entity(model='plane', scale=Vec3(mapData.width, 0, mapData.height), x=0, y=0, z=0, color=color.blue, collider='box')
+        center = Entity(model='plane', scale=Vec3(1, 0, 1), x=0, y=1, z=0, color=color.yellow, collider='box')
+        for node in mapData.level_map.values():
+            cell_x = node.pos[0] - (mapData.width / 2) + 0.5
+            cell_z = -(node.pos[1] - (mapData.height / 2) + 0.5 )
+            if not node.get_neighbour(0):
+                Entity(
+                    model='cube', scale=Vec3(1, 2, 0.1),
+                    position=(cell_x, 1, cell_z + 0.5),
+                    color=color.red, collider='box'
+                )
+
+            if not node.get_neighbour(1):
+                Entity(
+                    model='cube', scale=Vec3(0.1, 2, 1),
+                    position=(cell_x + 0.5, 1, cell_z),
+                    color=color.red, collider='box'
+                )
+
+            if not node.get_neighbour(2):
+                Entity(
+                    model='cube', scale=Vec3(1, 2, 0.1),
+                    position=(cell_x, 1, cell_z - 0.5),
+                    color=color.red, collider='box'
+                )
+
+            if not node.get_neighbour(3):
+                Entity(
+                    model='cube', scale=Vec3(0.1, 2, 1),
+                    position=(cell_x - 0.5, 1, cell_z),
+                    color=color.red, collider='box'
+                )
+
+    def game(self) -> None:
+        app = Ursina()
+        self.createMap(self.levels[0])
+        camera.position = (0, 50, 0)
+        camera.rotation_x = 90
+        camera.rotation_y = 0
+        camera.rotation_z = 0
+        app.run()
