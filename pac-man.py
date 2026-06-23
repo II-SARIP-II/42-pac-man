6b9ee98e8ef513c14c3d94620973d531371587f0
pgougne <paris.gougne.05@gmail.com>
2026-06-23 11:20:33 +0200

    gameState initialization

--- pac-man.py
+++ pac-man.py
@@ -1,10 +1,18 @@
 from src.models.config import ConfigFileValidation
 from src.utils_io import load_json_file
+from src.GameState import GameState
 
 
 def main() -> None:
     config = ConfigFileValidation(**load_json_file("./config/config.json"))
-    gameStat(config)
+    GameState(config.highscore_filename,
+              config.levels,
+              config.lives,
+              config.points_per_pacgum,
+              config.points_per_ghost,
+              config.seed,
+              config.level_max_time
+              )
 
 if __name__ == "__main__":
     main()
