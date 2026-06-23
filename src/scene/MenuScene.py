244b9073169dd05a81b8a465f6b760900b326920
jvacossi <jvacossi@student.42lyon.fr>
2026-06-23 18:30:14 +0200

    refactor (MenuScene): create various functions that creates buttons and background

--- src/scene/MenuScene.py
+++ src/scene/MenuScene.py
@@ -11,24 +11,36 @@ class MenuScene(Entity):
         super().__init__()
 
         self.game_state = game_state
+        self.createButtons()
+        self.createBackground()
 
+    def createBackground(self) -> None:
         Entity(model='plane',
                scale=Vec3(20, 1, 20),
                position=Vec3(0, 0, 0),
-               color=color.orange,
+               color=color.black,
                collider='box',
                parent=self
                )
 
+    def createButtons(self) -> None:
+        self.buttonPlay()
+
+    def buttonPlay(self) -> None:
         self.button_game = Button(
-                model='plane',
-                text='game',
-                scale=Vec3(5, 1, 5),
-                position=Vec3(0, 1, 0),
-                color=color.blue,
-                collider='box',
-                parent=self
-                )
+            model='plane',
+            text='PLAY',
+            scale=Vec3(5, 1, 2),
+            position=Vec3(0, 1, 1),
+            collider='box',
+            parent=self,
+            color=color.blue
+            )
+
+        if self.button_game.text_entity:
+            self.button_game.text_entity.color = color.yellow
+            self.button_game.text_entity.rotation_x = 90
+            self.button_game.text_entity.y = 0.05
 
         self.button_game.on_click = (
             lambda: self.game_state.display_scene(EnumScene.GAME))
