81c31b0b0793e04ef2a101640729a9c1b8135e0c
pgougne <paris.gougne.05@gmail.com>
2026-07-07 11:59:01 +0200

    fix: display correctly lives icons

--- src/scene/LivesLayout.py
+++ src/scene/LivesLayout.py
@@ -1,4 +1,4 @@
-from ursina import color, Vec2, Entity, camera, destroy
+from ursina import color, Vec2, Entity, camera, destroy, Text
 from typing import TYPE_CHECKING, Any
 from src.GameEngine import EnumScene
 
@@ -22,10 +22,53 @@ class LivesLayout(Entity):
         self.game_engine = game_engine
         self.infinite = False
         self.life_entities: list[Entity] = []
+
+        self.text_entity = Text(
+            text="",
+            position=Vec2(0.12, 0),
+            color=color.white,
+            parent=self,
+            scale=1.5
+        )
+
         self.displayLives()
 
+    def clear_icons(self) -> None:
+        """Supprime proprement toutes les entités d'icônes actuelles."""
+        for icon in self.life_entities:
+            destroy(icon)
+        self.life_entities.clear()
+
     def displayLives(self) -> None:
-        if not self.infinite:
+        self.clear_icons()
+
+        if self.infinite:
+            self.text_entity.text = ""
+            life_icon = Entity(
+                model="quad",
+                scale=(0.1, 0.1),
+                position=Vec2(0.06, 0),
+                texture="assets/images/infinity_yellow.png",
+                parent=self,
+                color=color.yellow
+            )
+            self.life_entities.append(life_icon)
+            return
+
+        if self.lives > 9:
+            life_icon = Entity(
+                model="quad",
+                scale=(0.05, 0.05),
+                position=Vec2(0.06, 0),
+                texture="assets/images/pacman.png",
+                parent=self,
+                color=color.yellow,
+            )
+            self.life_entities.append(life_icon)
+            self.text_entity.text = f"x{self.lives}"
+
+        else:
+            self.text_entity.text = ""
             for i in range(self.lives):
                 life_icon = Entity(
                     model="quad",
@@ -36,28 +79,15 @@ class LivesLayout(Entity):
                     color=color.yellow,
                 )
                 self.life_entities.append(life_icon)
-        else:
-            life_icon = Entity(
-                    model="quad",
-                    scale=(0.1, 0.1),
-                    position=Vec2(0.06, 0),
-                    texture="assets/images/infinity_yellow.png",
-                    parent=self,
-                    color=color.yellow
-                )
-            self.life_entities.append(life_icon)
 
     def infiniteLive(self) -> None:
-        for i in range(len(self.life_entities)):
-            last_icon = self.life_entities.pop()
-            destroy(last_icon)
         self.infinite = not self.infinite
         self.displayLives()
 
     def loseLife(self) -> None:
-        if self.lives > 0 and self.life_entities:
+        if self.lives > 0:
             self.lives -= 1
-            last_icon = self.life_entities.pop()
-            destroy(last_icon)
+            self.displayLives()
+
         if self.lives <= 0:
             self.game_engine.displayScene(EnumScene.LOSE)
