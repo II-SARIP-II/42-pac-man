c3c0711249298cf1a2eaaf66afa786eb73ab2596
jvacossi <jvacossi@student.42lyon.fr>
2026-07-08 18:04:39 +0200

    refacto: class Player, fix bugs and divide update method

--- src/core/Player.py
+++ src/core/Player.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING
 
-from ursina import Vec3, color, destroy, time
+from ursina import Vec3, color, destroy, time, invoke
 
 from src.core.Character import Character
 from src.core.Node import Node
@@ -19,10 +19,12 @@ class Player(Character):
             width: int,
             height: int
             ) -> None:
+
         if width % 2 != 0:
             self.start_pos = (width // 2, height // 2)
         else:
             self.start_pos = (width // 2 - 1, height // 2)
+
         super().__init__(
             model="sphere",
             scale=Vec3(0.7, 0.7, 0.7),
@@ -41,6 +43,7 @@ class Player(Character):
         self.score = 0
         self.game_scene = parent
         self.game_data = game_data
+
         self.get_eaten = False
 
     def loseLife(self) -> None:
@@ -50,7 +53,9 @@ class Player(Character):
             )
         self.current_node = self.getNode(self.start_pos)
         self.target_node = self.current_node
-        self.get_eaten = True
+
+        if not self.get_eaten:
+            self.flashingPlayer()
 
     def getPlayerPos(self) -> Vec3:
         return self.get_position(relative_to=self.game_scene)
@@ -61,81 +66,81 @@ class Player(Character):
             interval: float = 0.15
             ) -> None:
 
-        self.get_eaten = False
-        light_yellow = color.black
-        original_color = color.yellow
+        self.get_eaten = True
 
         max_steps = int(duration/interval)
 
         def toggle_color(step: int) -> None:
             if step >= max_steps:
-                self.color = original_color
+                self.color = color.yellow
+                self.get_eaten = False
                 return
 
-            if step % 2 == 0:
-                self.color = original_color
-            else:
-                self.color = light_yellow
+            self.color = color.yellow if step % 2 == 0 else color.black
 
-            from ursina import invoke
             invoke(toggle_color, step + 1, delay=interval)
 
         toggle_color(0)
 
-    def update(self) -> None:
-        if self.get_eaten:
-            self.flashingPlayer()
+    def _handleDirectionChange(self) -> None:
+        if self.wish_direction >= 0:
+            wish_dir_neighbour = self.current_node.neighbours[
+                self.wish_direction
+                ]
 
+            if wish_dir_neighbour is not None:
+                self.current_direction = self.wish_direction
+                neighbour = self.getNode(wish_dir_neighbour)
+                self.target_node = neighbour
+
+            elif wish_dir_neighbour is None:
+                curr_dir_neighbour = self.current_node.neighbours[
+                    self.current_direction
+                ]
+                if curr_dir_neighbour is not None:
+                    neighbour = self.getNode(curr_dir_neighbour)
+                    self.target_node = neighbour
+
+    def _handleMovement(self) -> None:
+        opposite_direction = (self.current_direction + 2) % 4
+
+        if self.wish_direction == opposite_direction:
+            tmp = self.target_node
+            self.target_node = self.current_node
+            self.current_node = tmp
+            self.current_direction = self.wish_direction
+
+        target_vector = convertPosToVec(self.target_node.pos, self.size)
+        vector_to_target = target_vector - self.position
+
+        if vector_to_target and vector_to_target.length_squared() > 0:
+            direction = vector_to_target.normalized()
+            distance_left = vector_to_target.length()
+
+            step = self.speed * time.dt
+
+            if distance_left <= step:
+                self.position = target_vector
+                self.current_node = self.target_node
+            else:
+                self.position += direction * step
+
+
+    def update(self) -> None:
         if self.current_node.item:
             self.eatItem(self.current_node)
 
         if self.current_node == self.target_node:
-            if self.wish_direction >= 0:
-                wish_dir_neighbour = self.current_node.neighbours[
-                    self.wish_direction
-                    ]
-
-                if wish_dir_neighbour is not None:
-                    self.current_direction = self.wish_direction
-                    neighbour = self.getNode(wish_dir_neighbour)
-                    self.target_node = neighbour
-
-                elif wish_dir_neighbour is None:
-                    curr_dir_neighbour = self.current_node.neighbours[
-                        self.current_direction
-                    ]
-                    if curr_dir_neighbour is not None:
-                        neighbour = self.getNode(curr_dir_neighbour)
-                        self.target_node = neighbour
-
-        if self.current_node != self.target_node:
-            opposite_direction = (self.current_direction + 2) % 4
-
-            if self.wish_direction == opposite_direction:
-                tmp = self.target_node
-                self.target_node = self.current_node
-                self.current_node = tmp
-                self.current_direction = self.wish_direction
-
-            target_vector = convertPosToVec(self.target_node.pos, self.size)
-            vector_to_target = target_vector - self.position
-
-            if vector_to_target and vector_to_target.length_squared() > 0:
-                direction = vector_to_target.normalized()
-                distance_left = vector_to_target.length()
-
-                step = self.speed * time.dt
-
-                if distance_left <= step:
-                    self.position = target_vector
-                    self.current_node = self.target_node
-                else:
-                    self.position += direction * step
+            self._handleDirectionChange()
+        else:
+            self._handleMovement()
 
     def eatItem(self, node: Node) -> None:
-        if node.item:
-            self.game_data.addScore(node.item.score)
-            destroy(node.item)
-            node.item = None
-            self.game_scene.current_nb_pacgum -= 1
-            self.game_scene.isTheLevelFinished()
+        if not node.item:
+            return
+
+        self.game_data.addScore(node.item.score)
+        destroy(node.item)
+        node.item = None
+        self.game_scene.current_nb_pacgum -= 1
+        self.game_scene.isTheLevelFinished()
